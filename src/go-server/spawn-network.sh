58bce69969ba93176f1a2aff5cf09172abf7f946
alejandro <aservet1@binghamton.edu>
2021-08-06 20:01:57 -0400

    [ to [[ bug fix

--- src/go-server/spawn-network.sh
+++ src/go-server/spawn-network.sh
@@ -6,7 +6,7 @@ set -e
 #  have all the nodes also read from the urls file so the network is a fully connected graph
 #    or do they even need that? if the client just has a list of all the nodes in the network, it can come up with its own path and then just have to send them to each other as general http URLs, right?
 
-if [ -z $@ ]; then
+if [[ -z $@ ]]; then
 	echo "usage: $0 port1 port2 ... portN"
 	echo "  will spawn a server running on each of these ports"
 	exit 2
