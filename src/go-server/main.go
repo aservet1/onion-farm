89f4262795cbf3f168a990f43963a683b857a4d6
alejandro <aservet1@binghamton.edu>
2021-08-06 20:10:57 -0400

    successully implemented a very basic message relay system

--- src/go-server/main.go
+++ src/go-server/main.go
@@ -4,8 +4,8 @@ import (
 	"os"
 	"fmt"
 	"log"
-	"time"
 	"bytes"
+	"net/url"
 	"strconv"
 	"net/http"
 	"io/ioutil"
@@ -15,7 +15,13 @@ import (
 
 var NODE_ID int; // find out how to make this const but dynamically defined (ie defined by command line args at runtime, but unchangeable after that)
 
-func __nodePrint(msg string) {
+type RelayMessage struct {
+	Msg string `json:"msg"`
+	RelayIndex int `json:relayIndex`
+	NodeURLs [3]string `json:nodeURLs`// 3 nodes per relay (eventually make this value dynamic) 
+}
+
+func _nodePrint(msg string) {
 	fmt.Printf("Node %d: %s\n", NODE_ID, msg)
 }
 
@@ -23,45 +29,52 @@ func handleRequests(PORT int) {
 	fmt.Printf("listening for requests on port %d ...\n", PORT);
 
 	myRouter := mux.NewRouter().StrictSlash(true);
-	// myRouter.HandleFunc("/", homePage).Methods("GET");
-	myRouter.HandleFunc("/api/sendMessage", sendMessage).Methods("POST");
-	myRouter.HandleFunc("/api/receiveMessage", receiveMessage).Methods("POST");
+	myRouter.HandleFunc("/api/relay", Relay).Methods("POST");
 
 	log.Fatal ( http.ListenAndServe( fmt.Sprintf(":%d", PORT), myRouter) );
 }
 
-func sendMessage (
-  writer http.ResponseWriter,
-  request *http.Request
-) {
+func ParseRelayMessage( writer http.ResponseWriter, request *http.Request ) RelayMessage {
+	var relaymsg RelayMessage
 
-	body := map[string]string{"filename":"hello.txt","textBody":".hellow0rld."}
-	json_data, err := json.Marshal(body)
+	b, err:= ioutil.ReadAll(request.Body)
+	defer request.Body.Close()
 	if err != nil {
-		log.Fatal(err);
+		http.Error(writer, err.Error(), 500)
+		return relaymsg
+	}
+	err = json.Unmarshal(b, &relaymsg)
+	if err != nil {
+		http.Error(writer, err.Error(), 500)
+		return relaymsg
 	}
 
-	Url, + := url.Parse("http://localhost:8081") // make this URL dynamically chosen, based on the whole dimensions and message information of the onion message
-	Url.Path += "/api/receiveMessage"
-	request, _ := http.Post(urL, "application/json", bytes.NewBuffer(json_data))
-
-	//- get a body of json data in this post request
-
-	res, err := http.DefaultClient.Do(request)
-	if err != nil {
-		fmt.Println(err)
-		return "error happened"
-	}
+	return relaymsg
 }
 
-func receiveMessage (
-  writer http.ResponseWriter,
-  request *http.Request
-) {
-	__nodePrint("Received message at " + string(time.Now()))
-	fmt.Println("Here's the body: ")
-	body := request.Body
-	fmt.Println ( body )
+func Relay (writer http.ResponseWriter, request *http.Request) {
+	relayMsg := ParseRelayMessage(writer, request)
+	index := relayMsg.RelayIndex
+	if index >= len(relayMsg.NodeURLs) {
+		_nodePrint("Received message: " + relayMsg.Msg)
+		return
+	}
+
+	_nodePrint("relaying message to " + relayMsg.NodeURLs[index] + "...")
+	NextURL, _ := url.Parse(relayMsg.NodeURLs[index])
+	NextURL.Path += "/api/relay"
+
+	relayMsg.RelayIndex = relayMsg.RelayIndex + 1
+	json_data, err := json.Marshal(relayMsg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//todo: actually read and work with the response to this
+	_, err2 := http.Post(NextURL.String(), "application/json", bytes.NewBuffer(json_data))
+	if err2 != nil {
+		log.Fatal(err)
+	}
 }
 
 func main() {
